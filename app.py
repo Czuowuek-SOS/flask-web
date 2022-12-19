fac1f027a8cf4e7f22a7fff65d0872c75bb54bbb
Czuowuek-SOS <jarozoliborski@outlook.com>
2022-12-19 22:22:12 +0100

    modified:   app.py 	new file:   static/uploads/KingaDumda_Dyskont.mp4 	new file:   "static/uploads/Ord\305\274azm.mp4" 	new file:   templates/about.html 	new file:   templates/contact.html 	modified:   templates/films.html 	deleted:    user.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from fileinput import filename
 
-import json
+from flask_sqlalchemy import SQLAlchemy
 
 # from users import User, db
+
 
 UPLOAD_FOLDER = "static/uploads"
 
@@ -11,6 +12,23 @@ app = Flask(__name__)
 # app.config['SERVER_NAME'] = "localhost:6090"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config["SLQALCHEMY_DATABASE_URI"] = "sqlite:///accounts.sqlite3"
+
+db = SQLAlchemy(app)
+
+class Accounts(db.Model):
+    id = db.Column('account_id', db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(50))  
+    pasword = db.Column(db.String(200))
+    ip = db.Column(db.String(10))
+
+    def __init__(self, name, email, password, ip):
+        self.name = name
+        self.email = email
+        self.pasword = password
+        self.ip = ip
+db.create_all()
 
 
 @app.route("/", methods=["GET"])
@@ -22,13 +40,29 @@ def home():
 def films():
     return render_template("films.html")
 
-@app.route("/login", methods=["GET"])
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/signin", methods=["POST", "GET"])
 def login():
+
+    if request.method == "POST":   
+        print("post")
+        
     return render_template("/log.html")
+
+@app.route("/myaacount")
+def myaccount():
+    return 'sesx'
 
 @app.route("/myip", methods=["GET"])
 def myip():
     return jsonify({'ip': request.remote_addr}), 200
 
 if  __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
